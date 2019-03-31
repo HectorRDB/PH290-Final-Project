d2f886344f8325d47958753adce913aa4bf32208
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2019-03-31 10:34:49 -0700

    Update varimpact.R

--- R/varimpact.R
+++ R/varimpact.R
@@ -15,7 +15,11 @@ rm(pckgs)
 # Load data ----
 df <- read_csv(here("data", "sample_ga_data_binomial.csv"),
                col_types = paste0(rep("n", 23), collapse = ""))[, -1]
+
+# We have to remove two variables because there is not enought support in the
+# data to consider them continuous before binning them. It migth also happen for
+# tmleshift, we will see
 vim <- varimpact(Y = as.integer(df$y),
-                 data = df %>% select(-y) %>% as.data.frame())
+                 data = df %>% select(-y, -dhc, -fun_ht_anc1) %>% as.data.frame())
 
 
