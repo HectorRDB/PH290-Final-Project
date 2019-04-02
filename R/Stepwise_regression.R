8b0de04987d32aa90a5d767fddb018912e22ac69
Jennifer Wiederspahn <jennifer@Jennifers-MBP.lan>
2019-04-02 16:50:40 -0700

    used step functionto drop coefficients based on AIC

--- R/Stepwise_regression.R
+++ R/Stepwise_regression.R
@@ -1,15 +1,19 @@
 library(stats)
 library(here)
-sample_data <- read.csv(here("data", "sample_ga_data.csv"))
+library(texreg)
+#sample_data <- read.csv(here("data", "sample_ga_data_binomial.csv"))
+sample_data <- read.csv("data/sample_ga_data_binomial.csv")
 head(sample_data)
 
-train_sample_data <- sample_data[ , !(colnames(sample_data) %in% "m_age_anc1")]
+train_sample_data <- sample_data[ , !(colnames(sample_data) %in% "X")]
 
 
-min.model = lm(sample_data$m_age_anc1 ~ ., data=sample_data)
-biggest <- formula(lm(sample_data$m_age_anc1~.,sample_data))
+min.model = glm(y ~ ., data=train_sample_data, family=binomial)
+biggest <- formula(glm(y ~.,train_sample_data, family=binomial))
 biggest
 fwd.model = step(min.model, direction='forward', scope=biggest)
 summary(fwd.model)
+#from Hector fullmod = glm(y ~ .,family=binomial)
 
-
+step_model<- step(min.model)
+texreg(step_model)
