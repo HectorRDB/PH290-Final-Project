70fdce515e0eab10568a9d10981f2a58163509e3
Jennifer Wiederspahn <jennifer@Jennifers-MBP.lan>
2019-04-02 22:31:32 -0700

    ranked based on p-value

--- R/Stepwise_regression.R
+++ R/Stepwise_regression.R
@@ -11,9 +11,14 @@ train_sample_data <- sample_data[ , !(colnames(sample_data) %in% "X")]
 min.model = glm(y ~ ., data=train_sample_data, family=binomial)
 biggest <- formula(glm(y ~.,train_sample_data, family=binomial))
 biggest
+# forward stepwise regression
 fwd.model = step(min.model, direction='forward', scope=biggest)
 summary(fwd.model)
-#from Hector fullmod = glm(y ~ .,family=binomial)
 
+# order coefficients based on p-value 
+ranked_by_pvalue <- summary(fwd.model)$coefficients[, c(1,4)][order(summary(fwd.model)$coefficients[,4]),]
+ranked_by_pvalue
+
+# variable selection based on AIC 
 step_model<- step(min.model)
 texreg(step_model)
