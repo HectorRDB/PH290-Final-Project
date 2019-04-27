116bbf6e8b0e6894fb053e901025fb1241c5cd01
abenite <alejandra_benitez@berkeley.edu>
2019-04-27 11:58:19 -0700

    runs for continuous and cat vars

--- R/vimshift_ph290_project.R
+++ R/vimshift_ph290_project.R
@@ -55,20 +55,20 @@ g_learner <- sl_lrn_dens
 #g_learner <- hal_dens
 learner_list <- list(Y = Q_learner, A = g_learner)
 
-new_tmle = function(likelihd, update, tmle_task) {
-
+new_tmle = function(likelihd, update, tmle_task, tmle_spec, updater) {
+  
   targeted_likelihood <- Targeted_Likelihood$new(likelihd, update)
-
+  
   # define parameter
   tmle_params <- tmle_spec$make_params(tmle_task, targeted_likelihood)
   updater$tmle_params <- tmle_params
   ate <- tmle_params[[1]]
-
+  
   # fit tmle update
   tmle_fit <- fit_tmle3(tmle_task, targeted_likelihood, list(ate), update, max_it)
   #out = c(tmle_fit$summary$tmle_est, tmle_fit$summary$se, tmle_fit$summary$lower, tmle_fit$summary$upper)
   return(tmle_fit)
-
+  
 }
 
 get_sd  = function(tm_fit, stat1, stat2) {
@@ -76,25 +76,12 @@ get_sd  = function(tm_fit, stat1, stat2) {
   sd1 = sqrt(var(tm_fit$estimates[[1]]$IC)/length(tm_fit$estimates[[1]]$IC))
   sd2 = sqrt(var(tm_fit$estimates[[2]]$IC)/length(tm_fit$estimates[[2]]$IC))
   sd3 = sqrt(var(tm_fit$estimates[[3]]$IC)/length(tm_fit$estimates[[3]]$IC))
-
+  
   test_sd = sqrt(var(tm_fit$estimates[[stat2]]$IC - tm_fit$estimates[[stat1]]$IC)/nrow(tm_fit$estimates[[stat2]]$IC))
   test_stat = ((tm_fit$estimates[[stat2]]$psi) - (tm_fit$estimates[[stat1]]$psi)) /test_sd
   ate = tm_fit$estimates[[stat2]]$psi - tm_fit$estimates[[stat1]]$psi
   pv = 2 * pnorm(-abs(test_stat))
   
-
-  # 2 * pnorm(-abs(test_stat))
-
-  # get_sd(tmle_fit, 1, 2)
-  # get_sd(tmle_fit, 2, 3)
-  #
-  # sqrt((sd1^1) + (sd2^2))
-  #
-  # sqrt(var(tmle_fit$estimates[[4]]$IC)/nrow(tmle_fit$estimates[[4]]$IC))
-  #
-  # estimates = sapply(tmle_fit$estimates[1:3], function(x) x$psi)
-  #
-
   ## MSM ##
   var_D <- cov(tm_fit$estimates[[4]]$IC)
   n <- nrow(tm_fit$estimates[[4]]$IC)
@@ -103,19 +90,13 @@ get_sd  = function(tm_fit, stat1, stat2) {
   beta_stat = tm_fit$estimates[[4]]$psi[2] / (se[2])
   pval_beta = 2 * pnorm(-abs(beta_stat))
   pval = 2 * pnorm(-abs(test_stat))
-
+  
   #return(c(pval_beta, pval))
   return(c(ate, pval, test_sd, beta_stat, pval_beta, se))
 }
 
-
 Xcont_date = subset(train, select = c(ga_at_anc1_by_lmp, ga_anc1_by_edd, ga_at_deliv_lmp, ga_at_delivery_by_ga_anc1, 
                                       ga_at_delivery_by_edd_anc1, ga_at_delivery_by_fh_anc1))
-
-##### now try for our data #####
-X = subset(dat, select = -c(dhc, y))
-## removed variables
-##had to remove categorical that were non binary
 
 Xcont = subset(train, select = -c(m_stature, fuel, enough_food, ever_no_food, run_out_food, not_enough_food,
                                   hh_smoker, alc, sex, dhc, y,
@@ -129,10 +110,7 @@ train[, names(Xcont)] <- Xcont + runif(0, 0.1, n = nrow(Xcont))
 Wnames = c(names(Xcont), names(Xcont_date))
 Wnames_cat = names(Xcat)
 
-#center and scale data first if
-#pvals_beta = rep(NA, length(Wnames))
-#pvals = rep(NA, length(Wnames))
-
+ ### stoachasti txt regime for continuous vars only
 run_vim_shift = function(train, Wnames) {
   
   ates = list()
@@ -158,50 +136,18 @@ run_vim_shift = function(train, Wnames) {
     ates[[i]] = get_sd(tmle_fit, 2, 3)
     
   }
-  
   #load(file = "var_imp_vimpshift_26_apr_2019.Rdata")
   names(ates) = Wnames
   results_tab = round(do.call(rbind, ates), 6)
-  class(results_tab)
-  colnames(results_tab) <- c("ate", "pval", "test_sd", "beta_stat", "pval_beta", "se_intercept", "se_beta")
   #results_tab
+  colnames(results_tab) <- c("ate", "pval", "test_sd", "beta_stat", "pval_beta", "se_int", "se_beta")
+  results_tab
   ordered_tab = results_tab[order(abs(results_tab[,1]), decreasing = T),]
-  return(rownames(ordered_tab)) 
-
-ates = rep(NA, length(Wnames))
-for (i in 1:(length(Wnames))) {
-  dat = data.frame(dat)
-  node_list <- list(W = names(dat)[!(names(dat) %in% c(Wnames[i], "y", "X"))], A = Wnames[i], Y = "y")
-  node_list
-
-  #current_a = Xcont$fun_ht_anc1
-  current_a = dat[,which(names(dat) == Wnames[i])]
-  print(class(dat))
-  delta_grid <- c(-sd(current_a), 0,  sd(current_a))
-  #delta_grid <- c(-sd(current_a), 0,  sd(current_a))
-  delta_grid
-  #delta_grid <- seq(min(current_a), max(current_a), sd(current_a))
-  #seq(from = min(current_a), to = max(current_a), length.out = 3)
-
-  # initialize a tmle specification
-  tmle_spec <- tmle_vimshift_delta(shift_grid = delta_grid,
-                                   max_shifted_ratio = 3)
-  tmle_fit <- tmle3(tmle_spec, dat, node_list, learner_list)
-
-  ates[i] = get_sd(tmle_fit, 2, 3)
-
-  # pv = get_sd(tmle_fit, 2, 3)
-  # pvals_beta[i] = pv[1]
-  # pvals[i] = pv[2]
+  return(ordered_tab) 
 }
 
-# Wnames[index_order]
-# Wnames[pvals_beta <= .05]
-# Wnames[pvals_beta > 0.05]
-#save(pvals_beta, file = "pval_beta_vimshift.Rdata")
-#save(pvals, file = "pval_ttest_vimshift.Rdata")
-# Wnames[pvals <= .05]
-# Wnames[pvals > 0.05]
+#save(ates, file = "var_imp_vimpshift_26_apr_2019.Rdata")
+
 
 ##### NOW FOR CATEGORICAL VARIABLES ###
 
@@ -215,37 +161,51 @@ metalearner <- make_learner(Lrnr_nnls)
 sl <- Lrnr_sl$new(learners = stack, metalearner = metalearner)
 learner_list_cat <- list(Y = sl, A = sl)
 #dhc and fun_ht_1
-reg_ate = rep(NA, length(Wnames_cat))
-for (i in 1:length(Wnames_cat)) {
 
-  dat = data.frame(dat)
-  node_list <- list(W = names(dat)[!(names(dat) %in% c(Wnames_cat[i], "y", "X"))], A = Wnames_cat[i], Y = "y")
-  node_list
-
-  ########
-  tmle_spec <- tmle_ATE(1,0)
-
-  # define data
-  tmle_task <- tmle_spec$make_tmle_task(dat, node_list)
-
-  #this one is standard tmle (no c-tmle like approach)
-  initial_likelihood = tmle_spec$make_initial_likelihood(tmle_task, learner_list_cat)
-  updater <- tmle3_Update$new()
-  ########
-  reg_tmle = new_tmle(initial_likelihood, tmle3_Update$new(), tmle_task)
-  reg_ate[i] = reg_tmle$estimates[[1]]$psi
-  ########
-
+run_cat_vars = function(train, Wnames_cat) {
+  reg_ate = list()
+  for (i in 1:length(Wnames_cat)) {
+    
+    dat = data.frame(train)
+    node_list <- list(W = names(dat)[!(names(dat) %in% c(Wnames_cat[i], "y", "X"))], A = Wnames_cat[i], Y = "y")
+    node_list
+    
+    ########
+    tmle_spec <- tmle_ATE(1,0)
+    
+    # define data
+    tmle_task <- tmle_spec$make_tmle_task(dat, node_list)
+    
+    #this one is standard tmle (no c-tmle like approach)
+    initial_likelihood = tmle_spec$make_initial_likelihood(tmle_task, learner_list_cat)
+    updater <- tmle3_Update$new()
+    ########
+    reg_tmle = new_tmle(initial_likelihood, tmle3_Update$new(), tmle_task, tmle_spec, updater)
+    estim = reg_tmle$estimates[[1]]$psi
+    test_sd = sqrt(var(reg_tmle$estimates[[1]]$IC)/nrow(train))
+    test_stat = estim/test_sd
+    pval = 2 * pnorm(-abs(test_stat))
+    reg_ate[[i]] = c(estim, pval, test_sd)
+    ########
+  }
+  names(reg_ate) = Wnames_cat
+  results_tab = round(do.call(rbind, reg_ate), 6)
+  #results_tab
+  colnames(results_tab) <- c("ate", "pval", "test_sd")
+  results_tab
+  ordered_tab = results_tab[order(abs(results_tab[,1]), decreasing = T),]
+  return(ordered_tab) 
 }
 
+##combine rankings for cat vars and continuous vars
 
-run_vim_shift(train, Wnames)
+run_combined_var_imp = function(train, Wnames, Wnames_cat){
+  
+  conts = run_vim_shift(train, Wnames)
+  cat = run_cat_vars(train, Wnames_cat)
+  combined_ordered = sort(abs(c(conts[, 1], cat[, 1])), decreasing = T)
+  return(names(combined_ordered))
+  
+}
 
-#write.csv(final, file = "final_var_imp_decr.csv")
-##stepwise: verify what model it's choosing
-##in stepwise fashion, adding and subtracting vars
-##for which reason it added all
-## table 1 (shows the random forest results)
-## compare the stimates with the true data and wants to put next to ea other
-##
-
+run_combined_var_imp(train, Wnames, Wnames_cat)
