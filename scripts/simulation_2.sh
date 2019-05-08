021e3c1e64cda22cfed520aa26d806ed41be7221
Hector <hector.rouxdebezieux@berkeley.edu>
2019-05-08 11:16:17 -0700

    it will

--- scripts/simulation_2.sh
+++ scripts/simulation_2.sh
@@ -3,7 +3,5 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --nodes=1
 
-file=
-R CMD BATCH --no-save ${file}.R ${file}.out
-NA
 file='R/simulation_2'
+R CMD BATCH --no-save ${file}.R ${file}.out
--- scripts/simulation_5.sh
+++ scripts/simulation_5.sh
@@ -3,7 +3,5 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --nodes=1
 
-file=
-R CMD BATCH --no-save ${file}.R ${file}.out
-NA
 file='R/simulation_5'
+R CMD BATCH --no-save ${file}.R ${file}.out
